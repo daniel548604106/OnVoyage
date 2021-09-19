78e892699984ea4d9fabd1fb2ccca8cd4ec2a226
Daniel Yeh <daniel548604106@gmail.com>
2021-09-19 20:23:26 +0800

    ssr complete

---
+++ server/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+import App from "../src/App";
+
+const PORT = 8000;
+const app = express();
+
+app.use("^/$", (req, res, next) => {
+  fs.readFile(path.resolve("./build/index.html"), "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Some error happened");
+    }
+    return res.send(
+      data.replace(
+        '<div id="root"></div>',
+        `<div id="root">${ReactDOMServer.renderToString(<App />)}</div>`
+      )
+    );
+    // this will return text content of our index.html
+    // and we need to change the mounting place
+  });
+});
+
+// serving other static file from our build folder
+
+app.use(express.static(path.resolve(__dirname, "..", "build")));
+
+app.listen(PORT, () => {
+  console.log(`app launched on ${PORT}`);
+});
